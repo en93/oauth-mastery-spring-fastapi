9754422b35627ed5843f80973b39302f91a77811
ianba <ianbabington93@gmail.com>
2025-12-25 00:45:33 +1300

    more work in progress

---
+++ client-webapp/app/services/sign_up_service.py
@@ -0,0 +1,10 @@
+from app.models import SignUpForm
+from pwdlib import PasswordHash
+
+async def sign_up(sign_up_form: SignUpForm):
+    
+    # Salt password, hash it
+    # Move to utility function later
+    password_hash = PasswordHash.recommended()
+    hashed_password = password_hash.hash(sign_up_form.password)
+    return hashed_password
