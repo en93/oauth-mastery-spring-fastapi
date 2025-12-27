10221317db216c4c461760e7afd2253eb9cecad0
ianba <ianbabington93@gmail.com>
2025-12-27 15:09:45 +1300

    config changes

--- client-webapp/app/database.py
+++ client-webapp/app/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from .settings import settings
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/dbname"
+DATABASE_URL = f"postgresql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
